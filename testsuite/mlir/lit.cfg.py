9812a5bee20e871c77f9cef0009ae4ef56e70d65
Renato Golin <rengolin@systemcall.eu>
2020-06-16 16:16:38 +0100

    Fix check-verona-mlir

--- testsuite/mlir/lit.cfg.py
+++ testsuite/mlir/lit.cfg.py
@@ -27,15 +27,12 @@ config.suffixes = ['.mlir', '.verona']
 config.test_source_root = os.path.dirname(__file__)
 
 # test_exec_root: The root path where tests should be run.
-config.test_exec_root = os.path.join(config.verona_obj_root, 'test')
-
+config.test_exec_root = os.path.join(config.verona_obj_root, 'testsuite/mlir')
 config.substitutions.append(('%PATH%', config.environment['PATH']))
 config.substitutions.append(('%shlibext', config.llvm_shlib_ext))
 
 llvm_config.with_system_environment(
     ['HOME', 'INCLUDE', 'LIB', 'TMP', 'TEMP'])
-
-llvm_config.use_default_substitutions()
 
 # excludes: A list of directories to exclude from the testsuite. The 'Inputs'
 # subdirectories contain auxiliary inputs for various tests in their parent
@@ -46,7 +43,7 @@ config.excludes = ['Inputs', 'Examples', 'CMakeLists.txt', 'README.txt', 'LICENS
 config.test_source_root = os.path.dirname(__file__)
 
 # test_exec_root: The root path where tests should be run.
-config.test_exec_root = os.path.join(config.verona_obj_root, 'test')
+config.test_exec_root = os.path.join(config.verona_obj_root, 'testsuite/mlir')
 config.verona_tools_dir = os.path.join(config.verona_obj_root, 'dist')
 
 # Tweak the PATH to include the tools dir.
